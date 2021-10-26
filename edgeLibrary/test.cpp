5733776c49558aaa9492e98a9589b87b666102cd
Rahul Kaplesh <rahul.kaplesh@gmail.com>
2021-10-26 22:17:25 +0530

    Small code cleanup
    Signed-off-by: Rahul Kaplesh <rahul.kaplesh@gmail.com>

--- edgeLibrary/test.cpp
+++ edgeLibrary/test.cpp
@@ -3,38 +3,8 @@
  * correctly.
  ************************************************************************************/ 
 #include "gtest/gtest.h"
-#include "QtCore/QThread"
-
-static void ExecuteGTest()
-{ 
-    {
-        //perform checks to validate that Qt Eventloop is actually running
-        const QThread* pCurrent = QThread::currentThread();
-        ASSERT_TRUE(nullptr != pCurrent) << "This is no QThread";
-        const auto* pEventLoop = pCurrent->eventDispatcher();
-        ASSERT_TRUE(nullptr != pEventLoop) << "Eventloop is not running";
-    }
-    const int iRes = RUN_ALL_TESTS();
-    //terminate the Qt Eventloop and return GTest Result
-}
-
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
-
-    //std::unique_ptr<QThread> pGTest(QThread::create(ExecuteGTest));
-    //pGTest->start();
-
-    //const int iGTestRes = app.exec();
-    //bool bIsRunning = pGTest->isRunning();
-    //if (bIsRunning)
-    //{
-    //    //GTest thread is still running =>should not been happening....requesting exit
-    //    //Request exit with error code
-    //    pGTest->exit(-1);
-    //    pGTest->wait();
-    //}
-    //return iGTestRes;
-    //ExecuteGTest();
-    RUN_ALL_TESTS();
+    int res = RUN_ALL_TESTS();
 }
